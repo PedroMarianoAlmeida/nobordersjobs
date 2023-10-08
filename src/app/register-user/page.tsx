cda6306791b599c063557102213cbd0a4f8c98c1
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-07 23:20:17 -0400

    initial register-user page
    source: https://daisyui.com/components/hero/#hero-with-form

--- src/app/register-user/page.tsx
+++ src/app/register-user/page.tsx
@@ -1,5 +1,38 @@
 const RegisterUserPage = () => {
-  return <h2 className="text-zinc-50">Register User Page</h2>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+          <p className="py-6">
+            You logged but are not in our records. Please register to continue
+            (or log out to continue navigating as anonymous).
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">
+                  Check Username availabilities
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="username"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterUserPage;
