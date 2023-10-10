ea1d325157b15485e3d240d0df2afe112d34fa78
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-09 23:16:11 -0400

    add rich text editor

--- src/app/post-job/PostJobForm.tsx
+++ src/app/post-job/PostJobForm.tsx
@@ -1,9 +1,48 @@
 "use client";
 
 import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const PostJobForm = () => {
   const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleChange = (value: string) => {
+    setText(value);
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  console.log(text);
+
   return (
     <form className="card-body">
       <div className="form-control">
@@ -17,6 +56,19 @@ const PostJobForm = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           required
+        />
+      </div>
+
+      <div className="form-control h-60">
+        <label className="label">
+          <span className="label-text">Job Title</span>
+        </label>
+        
+        <ReactQuill
+          value={text}
+          modules={modules}
+          formats={formats}
+          onChange={handleChange}
         />
       </div>
 
--- src/app/post-job/page.tsx
+++ src/app/post-job/page.tsx
@@ -8,16 +8,8 @@ const PostJobPage = async () => {
 
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Post a job!</h1>
-          <p className="py-6">
-            Thanks for let the world know about this No Borders Job!
-          </p>
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <PostJobForm />
-        </div>
+      <div className="card flex-shrink-0 w-full max-w-xl shadow-2xl bg-base-100">
+        <PostJobForm />
       </div>
     </div>
   );
