22d9fb5dda428e3696612b67d4994015d59a16eb
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-14 23:50:40 -0400

    preparing curator profile form

---
+++ src/app/curator/edit-description/CuratorEditDescriptionForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface CuratorEditDescriptionFormProps {
+  name: string;
+  currentProfile: string;
+}
+const CuratorEditDescriptionForm = ({
+  name,
+  currentProfile,
+}: CuratorEditDescriptionFormProps) => {
+  return <></>;
+};
+
+export default CuratorEditDescriptionForm;
--- src/app/curator/edit-description/page.tsx
+++ src/app/curator/edit-description/page.tsx
@@ -1,7 +1,27 @@
-const CuratorEditDescriptionPage = () => {
+import { redirect } from "next/navigation";
+
+import { checkUserIsCurator } from "@/services/dataBaseService";
+import CuratorEditDescriptionForm from "./CuratorEditDescriptionForm";
+
+const CuratorEditDescriptionPage = async () => {
+  const getCurator = await checkUserIsCurator();
+
+  if (
+    !getCurator.success ||
+    !getCurator.isCurator ||
+    getCurator.curator === undefined
+  )
+    redirect("/");
+  const {
+    curator: { name, profile },
+  } = getCurator;
   return (
     <main>
       <h1>Curator Edit Description</h1>
+      <CuratorEditDescriptionForm
+        name={name}
+        currentProfile={profile}
+      />
     </main>
   );
 };
