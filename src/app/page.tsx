ec9b2abdc74306fd9f97414bb65980a3e86572e4
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-14 22:13:50 -0400

    add content on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-import { promoteUserToCurator } from "@/services/dataBaseService";
 import Link from "next/link";
 
 export default async function Home() {
-  //await promoteUserToCurator('admin')
   return (
-    <main>
-      <h1>Home Page</h1>
-      <Link href="/job/new">
-        <button className="btn btn-secondary">Post Job</button>
-      </Link>
-      <Link href="/job/list">
-        <button className="btn btn-secondary">See Jobs</button>
-      </Link>
+    <main className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">No Border Jobs</h1>
+          <p className="py-6">
+            Jobs in companies that not care where you are from, just how good
+            you are!
+          </p>
+
+          <Link href="/job/list">
+            <button className="btn btn-primary">See Open Positions</button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
