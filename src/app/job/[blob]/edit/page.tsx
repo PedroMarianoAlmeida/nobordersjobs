e1d69df4cf9d1132a76681f804ba1823c728718e
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-16 09:12:03 -0400

    add validation to only who create the job be able to change it

---
+++ src/app/job/[blob]/edit/page.tsx
@@ -0,0 +1,47 @@
+import {
+  getJopPostByBlob,
+  getUserNameByEmail,
+} from "@/services/dataBaseService";
+import { getServerSession } from "next-auth";
+
+const EditJobPage = async ({
+  params: { blob },
+}: {
+  params: { blob: string };
+}) => {
+  const job = await getJopPostByBlob(blob);
+
+  if (!job.success) return <div>Job not found</div>;
+  const {
+    title,
+    company,
+    body,
+    curator: { name: curator },
+  } = job.data;
+
+  const session = await getServerSession();
+
+  if (
+    !session ||
+    session.user === undefined ||
+    session.user.email === null ||
+    session.user.email === undefined
+  )
+    return <div>Not authorized</div>;
+
+  const userEmail = await getUserNameByEmail(session.user.email);
+  if (userEmail.success === false) return <div>Not authorized</div>;
+  const {
+    user: { name },
+  } = userEmail;
+
+  if (name !== curator) return <div>Not authorized</div>;
+
+  return (
+    <main>
+      <h1>Edit Job</h1>
+    </main>
+  );
+};
+
+export default EditJobPage;
