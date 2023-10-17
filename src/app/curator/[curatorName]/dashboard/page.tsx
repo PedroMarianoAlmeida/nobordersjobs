55020ff936130c68e4f7439fabad0e72afcfeaa6
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-16 22:29:19 -0400

    add self curator validation

--- src/app/curator/[curatorName]/dashboard/page.tsx
+++ src/app/curator/[curatorName]/dashboard/page.tsx
@@ -1,8 +1,30 @@
+import { checkUserIsCurator, getJobList } from "@/services/dataBaseService";
+import { redirect } from "next/navigation";
+
 const CuratorDashboardPage = async ({
   params: { curatorName },
 }: {
   params: { curatorName: string };
 }) => {
+  // This code was copied form edit-description page... put it in a reusable function
+  const getCurator = await checkUserIsCurator();
+
+  if (
+    !getCurator.success ||
+    !getCurator.isCurator ||
+    getCurator.curator === undefined
+  )
+    redirect("/");
+  // -----------------------------
+  const {
+    curator: { name },
+  } = getCurator;
+
+  if (name !== curatorName) redirect("/"); //Here is validated if the user is the same of the url
+
+  const jobs = await getJobList({ curator: name });
+  console.log(jobs);
+
   return (
     <main>
       <h1>{curatorName} Dashboard</h1>
