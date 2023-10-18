13b7207e94da5b4ea20f53391a51cc7f71269a5f
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-17 21:10:26 -0400

    add closed info on page details

--- src/app/job/[blob]/page.tsx
+++ src/app/job/[blob]/page.tsx
@@ -15,6 +15,7 @@ const JobDetailPage = async ({
     title,
     company,
     body,
+    isOpen,
     curator: { name: curator },
   } = job.data;
   const bodyParsed = parse(body);
@@ -23,7 +24,13 @@ const JobDetailPage = async ({
     <main className="min-h-screen bg-base-200 flex items-center">
       <div className="card mx-auto w-full max-w-5xl  shadow-xl">
         <div className="flex flex-col bg-base-100 rounded-xl gap-4 p-5 items-center">
-          <h1 className="text-2xl font-semibold mb-2 text-center">{title}</h1>
+          <h1
+            className={`text-2xl font-semibold mb-2 text-center ${
+              isOpen ? "" : "text-error"
+            }`}
+          >
+            {title}
+          </h1>
 
           <h2 className="text-center">{company}</h2>
 
@@ -35,7 +42,7 @@ const JobDetailPage = async ({
               </span>
             </Link>
           </div>
-
+          {isOpen ? <></> : <h2 className="text-error">Closed Position</h2>}
           <div className="card card-compact w-96 bg-base-100 shadow-xl border p-5">
             {bodyParsed}
           </div>
